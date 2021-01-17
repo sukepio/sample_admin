11da43e1cbce4abedbba50a3df4474a974ff177b
sukepio <naoto927sukena@gmail.com>
2021-01-17 13:38:34 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+  
+  devise_for :customers
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+end
