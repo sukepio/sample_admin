0aa02a2472401a82738e99aa739a3a09b2eaa3a7
sukepio <naoto927sukena@gmail.com>
2021-01-17 13:56:24 +0000

    [add] items controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   }
   
   devise_for :customers
+  
+  namespace :admin do
+    resources :items
+  end
+  
+  
+    resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
 end
